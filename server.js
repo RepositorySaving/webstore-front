043c8e1fc1e64c9acdaaf2332f09d8c1c3e2c24e
José Eduardo <zeedu.profissional@gmail.com>
2020-05-04 11:09:15 -0300

    BUG Fix | CSS Proxy Reverse not working

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(bodyParser.json());
-app.use('/static', express.static(path.join(__dirname, '/public')))
+app.use('/static', express.static(path.join(__dirname, '/../../public')))
 
 app.engine('hbs', expressHandlebars({
     handlebars: allowInsecurePrototypeAccess(handlebars),
